3a34adeea45559ce7fc143f768252bdf80c74395
Chege-Simon <petekim211@gmail.com>
2024-05-01 10:26:57 +0300

    backend api

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -1,7 +1,9 @@
 import PocketBase from 'pocketbase';
 import { writable } from 'svelte/store';
+ 
+const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://127.0.0.1:8090/';
+export const pb = new PocketBase(backendUrl); 
 
-export const pb = new PocketBase('http://127.0.0.1:8090'); 
 
 export const currentUser = writable(pb.authStore.model);
 
